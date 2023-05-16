343288f9f08a111b4cc250ef6242a781003705ea
davidcrab <davidcrabtree18@icloud.com>
2023-05-16 05:59:45 -0400

    init commit

---
+++ config.py
@@ -0,0 +1,8 @@
+COMPLETIONS_MODEL = "text-davinci-003"
+EMBEDDINGS_MODEL = "text-embedding-ada-002"
+CHAT_MODEL = 'gpt-3.5-turbo'
+TEXT_EMBEDDING_CHUNK_SIZE=300
+VECTOR_FIELD_NAME='content_vector'
+PREFIX = "moodfitdocs"  
+INDEX_NAME = "moodfit-index"
+OPENAI_API_KEY = "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"
